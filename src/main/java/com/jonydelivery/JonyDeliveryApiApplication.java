b1cefadd6cb9305c588f52884d5c8d69b630d2a8
srcascudo <bocadesiri>
2021-08-10 21:27:45 -0300

    Initial commit

---
+++ src/main/java/com/jonydelivery/JonyDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.jonydelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonyDeliveryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JonyDeliveryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonydelivery/JonyDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonydelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonyDeliveryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
